959932d6c668bac5e2b1bfb477f379f80c8caa85
Lai-YT <381xvmvbib@gmail.com>
2022-09-01 14:53:11 +0800

    feat: the application factory

---
+++ README.md
@@ -0,0 +1,3 @@
+# flask-tutorial
+
+A journey on the tutorial of [Flask 2.2.x](https://flask.palletsprojects.com/en/2.2.x/tutorial/)
---
+++ flaskr/__init__.py
@@ -0,0 +1,28 @@
+import os
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY = 'dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    _create_instance_path_if_not_exist(app)
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
+
+
+def _create_instance_path_if_not_exist(app):
+    os.makedirs(app.instance_path, exist_ok=True)
