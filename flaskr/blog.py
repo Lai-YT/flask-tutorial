dc82ad462f6a6458995f575a94d2d6fed4c7086a
Lai-YT <381xvmvbib@gmail.com>
2022-09-02 10:57:07 +0800

    feat: blog
    creation, update and deletion

---
+++ flaskr/blog.py
@@ -0,0 +1,89 @@
+from flask import (Blueprint, flash, g, redirect, render_template, request,
+                   url_for)
+from werkzeug.exceptions import abort
+
+from flaskr.auth import login_required
+from flaskr.db import get_db
+
+bp = Blueprint('blog', __name__)
+
+
+@bp.route('/')
+def index():
+    """Show all the posts."""
+    db = get_db()
+    posts = db.execute('''
+        SELECT p.id, title, body, created, author_id, username
+            FROM post p JOIN user u ON p.author_id = u.id
+            ORDER BY created DESC
+    ''').fetchall()
+    return render_template('blog/index.html', posts=posts)
+
+
+@bp.route('/create', methods=('GET', 'POST'))
+@login_required
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+
+        error = ''
+        if not title:
+            error = 'Title is required.'
+
+        if error:
+            flash(error)
+        else:
+            with get_db() as db:
+                db.execute(
+                    'INSERT INTO post (title, body, author_id) VALUES (?, ?, ?)',
+                    (title, body, g.user['id'])
+                ),
+            return redirect(url_for('blog.index'))
+    return render_template('blog/create.html')
+
+
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update(id: int):
+    post = get_post_by_id(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+
+        error = ''
+        if not title:
+            error = 'Title is required.'
+
+        if error:
+            flash(error)
+        else:
+            with get_db() as db:
+                db.execute('UPDATE post SET title = ?, body = ? WHERE id = ?',
+                           (title, body, id))
+            return redirect(url_for('blog.index'))
+    return render_template('blog/update.html', post=post)
+
+
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id: int):
+    get_post_by_id(id)
+    with get_db() as db:
+        db.execute('DELETE FROM post WHERE id = ?', (id,))
+    return redirect(url_for('blog.index'))
+
+
+def get_post_by_id(id: int, check_author=True):
+    post = get_db().execute('''
+        SELECT p.id, title, body, created, author_id, username
+            FROM post p JOIN user u ON p.author_id = u.id
+            WHERE p.id = ?
+    ''', (id,)).fetchone()
+
+    if post is None:
+        abort(404, f"Post id {id} doesn't exist.")
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+    return post
